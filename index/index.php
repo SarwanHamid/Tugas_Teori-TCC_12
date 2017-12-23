65a4055e61e4cfc9b65112104edf4f40a46a0208
SarwanHamid <noolkoma549@gmail.com>
2017-12-23 16:03:48 +0700

    php with docker-compose

---
+++ index/index.php
@@ -0,0 +1,8 @@
+<?php
+echo "<b>";
+echo "<h2>Tugas Teori TCC 12</h2>";
+echo "<br>";
+echo "<pre><h3>Nama 		: Sarwan Hamid<br>";
+echo "NIM 		: 155410026<br>";
+echo "Jurusan 	: Teknik Informatika</h3></pre>";
+echo "</b>";
